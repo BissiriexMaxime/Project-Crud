54bee32203777b052971a79296334cba7e23dd50
BissiriexMaxime <bissiriexm@gmail.com>
2024-02-22 11:08:04 +0100

    MàJ22/02/24

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,12 +2,12 @@
 
 namespace App\Controller;
 
-use App\Entity\Articles;
 use App\Form\CrudType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Request;
+use App\Entity\Articles;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
@@ -24,15 +24,27 @@ class MainController extends AbstractController
     }
 
      /**
-     * @Route("/create", name="create", methods: ["GET","POST"])
+     * @Route("/create", name="create", methods= {"GET","POST"})
      */
     public function create(Request $request): Response
     {
         $article = new Articles();
-        $form = $this->createForm(CrudType::class, $article)
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
-    }
+        $form = $this->createForm(CrudType::class, $article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sendDatabase = $this   ->getDoctrine()
+                                    ->getManager();
+            $sendDatabase->persist($article);
+            $sendDatabase->flush();
 
+            $this->addFlash("success", "L'article a bien été enregistré"); 
+            return $this->redirectToRoute('app_main');
+            }
+        return $this->render('main/createForm.html.twig', [
+            'controller_name' => 'MainController',
+            'form'=>$form->createView(),
+        ]);
+    
+
+}
 }
